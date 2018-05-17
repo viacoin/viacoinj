19d3ee1dd140bf8fe4a058ede7877aa8b8907353
Chainfailure <chainfailure@protonmail.com>
2018-05-17 20:48:59 +0200

    Viacoin: set mainnet magic

--- core/src/main/java/org/bitcoinj/params/MainNetParams.java
+++ core/src/main/java/org/bitcoinj/params/MainNetParams.java
@@ -42,7 +42,7 @@ public class MainNetParams extends AbstractBitcoinNetParams {
         p2shHeader = 5;
         segwitAddressHrp = "bc";
         port = 8333;
-        packetMagic = 0xf9beb4d9L;
+        packetMagic = 0x0f68c6cb;
         bip32HeaderPub = 0x0488B21E; //The 4 byte header that serializes in base58 to "xpub".
         bip32HeaderPriv = 0x0488ADE4; //The 4 byte header that serializes in base58 to "xprv"
 
